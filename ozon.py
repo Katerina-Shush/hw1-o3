34b7720d80a245a36a3b3a38408595615f144f99
Katerina-Shush <116030372+Katerina-Shush@users.noreply.github.com>
2023-02-10 22:59:40 +0300

    Update ozon.py

--- ozon.py
+++ ozon.py
@@ -1,11 +1,61 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import netcdf
 
 parser = argparse.ArgumentParser()
 parser.add_argument('longitude', metavar='LON', type=float, help='Longitude, deg')
 parser.add_argument('latitude',  metavar='LAT', type=float, help='Latitude, deg')
 
-if __name__ == "__main__":
-    args = parser.parse_args()
-    print(args.longitude, args.latitude)
+def solver(lon, lat):
+    with netcdf.netcdf_file('MSR-2.nc', mmap=False) as netcdf_file:
+        variables = netcdf_file.variables
+        lat_index = np.searchsorted(variables['latitude'].data, lat)
+        lon_index = np.searchsorted(variables['longitude'].data, lon)
+        data = variables['Average_O3_column'][:, lat_index, lon_index]
+
+        _dict = get_dict(data, lon, lat)
+        save_as_json(_dict)
+        save_plot(variables['time'].data, data)
+
+def get_dict(data, lon, lat):
+    _data = {
+        "coordinates": [lon, lat],
+        "jan": {
+            "min": float(data[::12].min()),
+            "max": float(data[::12].max()),
+            "mean": np.mean(data[::12])
+        },
+        "jul": {
+            "min": float(data[6::12].min()),
+            "max": float(data[6::12].max()),
+            "mean": np.mean(data[6::12])
+        },
+        "all": {
+            "min": float(data.min()),
+            "max": float(data.max()),
+            "mean": np.mean(data)
+        }
+    }
+
+    return _data
+
+def save_as_json(data):
+    with open('ozon.json', 'w') as fp:
+        json.dump(data, fp)
+
+def save_plot(time, data):
+    plt.figure(figsize=(30,20))
+    plt.plot(time[::12], data[::12], label = 'JAN')
+    plt.plot(time[6::12], data[6::12], label = 'JUL')
+    plt.plot(time, data, '-', label = 'ALL')
+    plt.savefig('ozon.png')
+
+
+args = parser.parse_args()
+lon = args.longitude
+lat = args.latitude
+solver(lon, lat)
